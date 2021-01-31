39d151643b435f54aa1fe6425d033739e3072b59
wzy0727 <33439095+wzy0727@users.noreply.github.com>
2021-01-31 20:34:14 +0800

    Update Action2.py

--- Lesson1/Action2.py
+++ Lesson1/Action2.py
@@ -1,25 +1,18 @@
-import numpy as np
+import pandas as pd
 
-persontype = np.dtype({
-     'names':['name','chinese','math','english'],
-     'formats':['S32','i','i','i']
-})
+Data = {'Chinese':[68,95,98,90,80],'Math':[65,76,86,88,90],'English':[30,98,88,77,90]}
+Score = pd.DataFrame(Data,index=['ZhangFei', 'GuanYu', 'LiuBei', 'DianWei', 'XuChu'], \
+     columns=['Chinese', 'Math', 'English'])
 
-peoples = np.array([
-     ("Zhangfei",68,65,30),("Guanyu",95,76,98),("Liubei",98,86,88),
-     ("Dianwei",90,88,77),("Xuchu",80,90,90)],dtype=persontype)
+df1=pd.DataFrame(Score.mean(),columns=['mean'])
+df2=pd.DataFrame(Score.min(),columns=['min'])
+df3=pd.DataFrame(Score.max(),columns=['max'])
+df4=pd.DataFrame(Score.var(),columns=['var'])
+df5=pd.DataFrame(Score.std(),columns=['std'])
 
-chinese_score = peoples['chinese']
-math_score = peoples['math']
-english_score = peoples['english']
+df=pd.concat((df1,df2,df3,df4,df5), axis = 1,ignore_index = False, join = 'outer')
+print(df)
 
-print("       |平局成绩" + "|最小成绩" + "|最大成绩" + "|  方差  " + "|标准差")
-print("chinese|  %5.2f |   %d   |   %d   | %5.2f | %5.2f "
-          %(np.mean(chinese_score), min(chinese_score), max(chinese_score), np.var(chinese_score), np.std(chinese_score)))
-print("math   |  %5.2f |   %d   |   %d   | %5.2f  | %5.2f "
-          %(np.mean(math_score), min(math_score), max(math_score), np.var(math_score), np.std(math_score)))
-print("english|  %5.2f |   %d   |   %d   | %5.2f | %5.2f "
-          %(np.mean(english_score), min(english_score), max(english_score), np.var(english_score), np.std(english_score)))
-
-ranking = sorted(peoples,key=lambda x:x[1]+x[2]+x[3], reverse=True)
+Score['Sum'] = Score.apply(lambda x: x.sum(), axis=1)
+ranking = Score.sort_values('Sum')
 print(ranking)
